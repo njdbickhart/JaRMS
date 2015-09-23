4dba91097b2c9b43dc413339c3df9d5f70988fef
njdbickhart <njdbickhart@gmail.com>
2015-09-23 17:52:44 -0400

    initial commit

---
+++ src/HistogramUtils/ChrHistogramFactory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HistogramUtils;
+
+/**
+ *
+ * @author Derek.Bickhart
+ */
+public class ChrHistogramFactory {
+    
+    
+    // Collect read midpoint position count arrays for each bam
+    
+    // Merge all counts into a separate position count array for the bams
+    
+    // Calculate the GC percentage per chromosome
+    
+    // Normalize using similar method to lowess smoother in Alkan
+    
+    // Do mean shift algorithm with three levels and increasing bin sizes -- this makes the partition
+    
+    // Segment the genome based on the partition signal -- two-pass segmentation in order to join smaller segments
+    
+    
+}
