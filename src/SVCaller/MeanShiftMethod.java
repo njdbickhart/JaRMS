cd29a4efbaea992e0c36e26d0591c59338bef5fa
njdbickhart <njdbickhart@gmail.com>
2015-09-27 20:51:03 -0400

    setting foundation for stat functions

---
+++ src/SVCaller/MeanShiftMethod.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SVCaller;
+
+import FastaUtils.GCWindowFactory;
+import HistogramUtils.ChrHistogramFactory;
+
+/**
+ *
+ * @author desktop
+ */
+public class MeanShiftMethod {
+    
+    public void Partition(ChrHistogramFactory chisto, GCWindowFactory gchisto){
+        
+    }
+}
---
+++ src/stats/TDistributionFunction.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stats;
+
+/**
+ * The T test to determine if a partition is significantly different from surrounding partitions
+ * @author desktop
+ */
+public class TDistributionFunction {
+    
+}
