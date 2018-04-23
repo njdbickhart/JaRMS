d517c234ff46b9b42048c31e5249e337712b3c69
Derek Bickhart <njdbickhart@gmail.com>
2018-04-23 08:50:40 -0500

    started profiler

---
+++ src/Profiler/Mops.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Profiler;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dbickhart
+ */
+public class Mops {
+    private static final Logger log = Logger.getLogger(Mops.class.getName());
+    private final double eps = 1e-100;
+    
+}
---
+++ src/Profiler/SampleList.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Profiler;
+
+import HistogramUtils.ChrHistogramFactory;
+import java.util.Map;
+
+/**
+ *
+ * @author dbickhart
+ */
+public class SampleList {
+    private Map<String, ChrHistogramFactory> data;
+    
+    
+}
